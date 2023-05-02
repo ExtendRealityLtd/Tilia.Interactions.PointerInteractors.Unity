0b49bab08ef6389514f5891e0f220035ca349e51
Harvey Ball <me@harveyball.com>
2023-05-02 14:20:10 +0100

    feat(Prefabs): add pointer grabber prefab
    The PointerGrabber casts a straight pointer that has an interactor
    embedded within the pointer cursor and can be used to grab objects
    at a distance. The pointer then switches to a fixed length upon grab
    and the length of the pointer can be controlled by a float axis value.

---
+++ Runtime/SharedResources/Scripts/PointerGrabberConfigurator.cs
@@ -0,0 +1,226 @@
+﻿namespace Tilia.Interactions.PointerInteractors
+{
+    using Tilia.Indicators.ObjectPointers;
+    using Tilia.Interactions.Interactables.Interactors;
+    using UnityEngine;
+    using Zinnia.Action;
+    using Zinnia.Data.Attribute;
+    using Zinnia.Extension;
+    using Zinnia.Rule.Collection;
+    using Zinnia.Tracking.Follow;
+
+    public class PointerGrabberConfigurator : MonoBehaviour
+    {
+        #region Facade Settings
+        [Header("Facade Settings")]
+        [Tooltip("The public facade.")]
+        [SerializeField]
+        [Restricted]
+        private PointerGrabberFacade facade;
+        /// <summary>
+        /// The public facade.
+        /// </summary>
+        public PointerGrabberFacade Facade
+        {
+            get
+            {
+                return facade;
+            }
+            set
+            {
+                facade = value;
+            }
+        }
+        #endregion
+
+        #region Reference Settings
+        [Header("Reference Settings")]
+        [Tooltip("The PointerFacade for the internal pointer.")]
+        [SerializeField]
+        [Restricted]
+        private PointerFacade pointer;
+        /// <summary>
+        /// The <see cref="PointerFacade"/> for the internal pointer.
+        /// </summary>
+        public PointerFacade Pointer
+        {
+            get
+            {
+                return pointer;
+            }
+            set
+            {
+                pointer = value;
+            }
+        }
+        [Tooltip("The InteractorFacade for the internal interactor.")]
+        [SerializeField]
+        [Restricted]
+        private InteractorFacade interactor;
+        /// <summary>
+        /// The <see cref="InteractorFacade"/> for the internal interactor.
+        /// </summary>
+        public InteractorFacade Interactor
+        {
+            get
+            {
+                return interactor;
+            }
+            set
+            {
+                interactor = value;
+            }
+        }
+        [Tooltip("The FloatAction to link to the Facade.LengthAxisAction.")]
+        [SerializeField]
+        [Restricted]
+        private FloatAction lengthAxisProxy;
+        /// <summary>
+        /// The <see cref="FloatAction"/> to link to the <see cref="Facade.LengthAxisAction"/>.
+        /// </summary>
+        public FloatAction LengthAxisProxy
+        {
+            get
+            {
+                return lengthAxisProxy;
+            }
+            set
+            {
+                lengthAxisProxy = value;
+            }
+        }
+        [Tooltip("The RuleContainerObservableList that controls pointer target validity.")]
+        [SerializeField]
+        [Restricted]
+        private RuleContainerObservableList targetValidityRules;
+        /// <summary>
+        /// The <see cref="RuleContainerObservableList"/> that controls pointer target validity.
+        /// </summary>
+        public RuleContainerObservableList TargetValidityRules
+        {
+            get
+            {
+                return targetValidityRules;
+            }
+            set
+            {
+                targetValidityRules = value;
+            }
+        }
+        [Tooltip("The container for the grab on enter logic.")]
+        [SerializeField]
+        [Restricted]
+        private GameObject grabOnEnterLogic;
+        /// <summary>
+        /// The container for the grab on enter logic.
+        /// </summary>
+        public GameObject GrabOnEnterLogic
+        {
+            get
+            {
+                return grabOnEnterLogic;
+            }
+            set
+            {
+                grabOnEnterLogic = value;
+            }
+        }
+        [Tooltip("The ObjectFollower to make the Interactor follow the Facade.FollowSource.")]
+        [SerializeField]
+        [Restricted]
+        private ObjectFollower interactorFollower;
+        /// <summary>
+        /// The <see cref="ObjectFollower"/> to make the Interactor follow the <see cref="Facade.FollowSource"/>.
+        /// </summary>
+        public ObjectFollower InteractorFollower
+        {
+            get
+            {
+                return interactorFollower;
+            }
+            set
+            {
+                interactorFollower = value;
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Configures the relevant settings for the internal Pointer.
+        /// </summary>
+        public virtual void ConfigurePointer()
+        {
+            Pointer.FollowSource = Facade.FollowSource;
+            Pointer.ActivationAction = Facade.ActivationAction;
+            LengthAxisProxy.RunWhenActiveAndEnabled(() => SetLengthAxisProxySource());
+            InteractorFollower.Sources.RunWhenActiveAndEnabled(() => SetObjectFollowerSource());
+        }
+
+        /// <summary>
+        /// Configures the relevant settings for the internal Interactor.
+        /// </summary>
+        public virtual void ConfigureInteractor()
+        {
+            Interactor.GrabAction = Facade.GrabAction;
+            Interactor.VelocityTracker = Facade.VelocityTracker;
+        }
+
+        /// <summary>
+        /// Configures the rules on the pointer.
+        /// </summary>
+        public virtual void ConfigurePointerRules()
+        {
+            if (TargetValidityRules.NonSubscribableElements.Count > 1)
+            {
+                TargetValidityRules.RunWhenActiveAndEnabled(() => TargetValidityRules.RemoveAt(1));
+            }
+
+            if (Facade.TargetValidity.Interface != null)
+            {
+                TargetValidityRules.RunWhenActiveAndEnabled(() => TargetValidityRules.Add(Facade.TargetValidity));
+            }
+
+            Pointer.RaycastRules = Facade.RaycastRules;
+        }
+
+        /// <summary>
+        /// Configures the state of the grab on enter logic container.
+        /// </summary>
+        public virtual void ConfigureGrabOnEnter()
+        {
+            GrabOnEnterLogic.SetActive(Facade.AutoGrabOnEnter);
+        }
+
+        protected virtual void OnEnable()
+        {
+            ConfigurePointer();
+            ConfigureInteractor();
+            ConfigurePointerRules();
+            ConfigureGrabOnEnter();
+        }
+
+        /// <summary>
+        /// Sets the source on the Length Axis Proxy.
+        /// </summary>
+        protected virtual void SetLengthAxisProxySource()
+        {
+            LengthAxisProxy.ClearSources();
+            if (Facade.LengthAxisAction != null)
+            {
+                LengthAxisProxy.AddSource(Facade.LengthAxisAction);
+            }
+        }
+
+        /// <summary>
+        /// Sets the source of the object follower to the <see cref="Facade.FollowSource"/>.
+        /// </summary>
+        protected virtual void SetObjectFollowerSource()
+        {
+            InteractorFollower.Sources.Clear();
+            if (Facade.FollowSource != null)
+            {
+                InteractorFollower.Sources.Add(Facade.FollowSource);
+            }
+        }
+    }
+}
---
+++ Runtime/SharedResources/Scripts/PointerGrabberFacade.cs
@@ -0,0 +1,278 @@
+﻿namespace Tilia.Interactions.PointerInteractors
+{
+    using UnityEngine;
+    using Zinnia.Action;
+    using Zinnia.Cast;
+    using Zinnia.Data.Attribute;
+    using Zinnia.Extension;
+    using Zinnia.Rule;
+    using Zinnia.Tracking.Velocity;
+
+    /// <summary>
+    /// The public interface into the PointerGrabber Prefab.
+    /// </summary>
+    public class PointerGrabberFacade : MonoBehaviour
+    {
+        #region Pointer Settings
+        [Header("Pointer Settings")]
+        [Tooltip("The source to get the internal pointer to follow.")]
+        [SerializeField]
+        private GameObject followSource;
+        /// <summary>
+        /// The source to get the internal pointer to follow.
+        /// </summary>
+        public GameObject FollowSource
+        {
+            get
+            {
+                return followSource;
+            }
+            set
+            {
+                followSource = value;
+                if (this.IsMemberChangeAllowed())
+                {
+                    OnAfterFollowSourceChange();
+                }
+            }
+        }
+        [Tooltip("The Action to enable and disable the pointer with.")]
+        [SerializeField]
+        private BooleanAction activationAction;
+        /// <summary>
+        /// The Action to enable and disable the pointer with.
+        /// </summary>
+        public BooleanAction ActivationAction
+        {
+            get
+            {
+                return activationAction;
+            }
+            set
+            {
+                activationAction = value;
+                if (this.IsMemberChangeAllowed())
+                {
+                    OnAfterActivationActionChange();
+                }
+            }
+        }
+        [Tooltip("The Action to attempt to get the pointer interactor to grab.")]
+        [SerializeField]
+        private BooleanAction grabAction;
+        /// <summary>
+        /// The Action to attempt to get the pointer interactor to grab.
+        /// </summary>
+        public BooleanAction GrabAction
+        {
+            get
+            {
+                return grabAction;
+            }
+            set
+            {
+                grabAction = value;
+                if (this.IsMemberChangeAllowed())
+                {
+                    OnAfterGrabActionChange();
+                }
+            }
+        }
+        [Tooltip("The Action control the fixed pointer length.")]
+        [SerializeField]
+        private FloatAction lengthAxisAction;
+        /// <summary>
+        /// The Action control the fixed pointer length.
+        /// </summary>
+        public FloatAction LengthAxisAction
+        {
+            get
+            {
+                return lengthAxisAction;
+            }
+            set
+            {
+                lengthAxisAction = value;
+                if (this.IsMemberChangeAllowed())
+                {
+                    OnAfterLengthAxisActionChange();
+                }
+            }
+        }
+        [Tooltip("The VelocityTracker to use when applying velocity to the Interactor.")]
+        [SerializeField]
+        private VelocityTracker velocityTracker;
+        /// <summary>
+        /// The <see cref="VelocityTracker"/> to use when applying velocity to the Interactor.
+        /// </summary>
+        public VelocityTracker VelocityTracker
+        {
+            get
+            {
+                return velocityTracker;
+            }
+            set
+            {
+                velocityTracker = value;
+                if (this.IsMemberChangeAllowed())
+                {
+                    OnAfterVelocityTrackerChange();
+                }
+            }
+        }
+        [Tooltip("Whether to attempt to automatically grab upon the pointer entering a valid target.")]
+        [SerializeField]
+        private bool autoGrabOnEnter;
+        /// <summary>
+        /// Whether to attempt to automatically grab upon the pointer entering a valid target.
+        /// </summary>
+        public bool AutoGrabOnEnter
+        {
+            get
+            {
+                return autoGrabOnEnter;
+            }
+            set
+            {
+                autoGrabOnEnter = value;
+                if (this.IsMemberChangeAllowed())
+                {
+                    OnAfterAutoGrabOnEnterChange();
+                }
+            }
+        }
+        #endregion
+
+        #region Rule Settings
+        [Header("Rule Settings")]
+        [Tooltip("Determines which targets are valid to the pointer.")]
+        [SerializeField]
+        private RuleContainer targetValidity;
+        /// <summary>
+        /// Determines which targets are valid to the pointer.
+        /// </summary>
+        public RuleContainer TargetValidity
+        {
+            get
+            {
+                return targetValidity;
+            }
+            set
+            {
+                targetValidity = value;
+                if (this.IsMemberChangeAllowed())
+                {
+                    OnAfterTargetValidityChange();
+                }
+            }
+        }
+        [Tooltip("Determines the rules for the pointer RayCast.")]
+        [SerializeField]
+        private PhysicsCast raycastRules;
+        /// <summary>
+        /// Determines the rules for the pointer RayCast.
+        /// </summary>
+        public PhysicsCast RaycastRules
+        {
+            get
+            {
+                return raycastRules;
+            }
+            set
+            {
+                raycastRules = value;
+                if (this.IsMemberChangeAllowed())
+                {
+                    OnAfterRaycastRulesChange();
+                }
+            }
+        }
+        #endregion
+
+        #region Reference Settings
+        [Header("Reference Settings")]
+        [Tooltip("The linked Internal Setup.")]
+        [SerializeField]
+        [Restricted]
+        private PointerGrabberConfigurator configuration;
+        /// <summary>
+        /// The linked Internal Setup.
+        /// </summary>
+        public PointerGrabberConfigurator Configuration
+        {
+            get
+            {
+                return configuration;
+            }
+            set
+            {
+                configuration = value;
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Called after <see cref="FollowSource"/> has been changed.
+        /// </summary>
+        protected virtual void OnAfterFollowSourceChange()
+        {
+            Configuration.ConfigurePointer();
+        }
+
+        /// <summary>
+        /// Called after <see cref="ActivationAction"/> has been changed.
+        /// </summary>
+        protected virtual void OnAfterActivationActionChange()
+        {
+            Configuration.ConfigurePointer();
+        }
+
+        /// <summary>
+        /// Called after <see cref="GrabAction"/> has been changed.
+        /// </summary>
+        protected virtual void OnAfterGrabActionChange()
+        {
+            Configuration.ConfigureInteractor();
+        }
+
+        /// <summary>
+        /// Called after <see cref="LengthAxisAction"/> has been changed.
+        /// </summary>
+        protected virtual void OnAfterLengthAxisActionChange()
+        {
+            Configuration.ConfigurePointer();
+        }
+
+        /// <summary>
+        /// Called after <see cref="VelocityTracker"/> has been changed.
+        /// </summary>
+        protected virtual void OnAfterVelocityTrackerChange()
+        {
+            Configuration.ConfigureInteractor();
+        }
+
+        /// <summary>
+        /// Called after <see cref="AutoGrabOnEnter"/> has been changed.
+        /// </summary>
+        protected virtual void OnAfterAutoGrabOnEnterChange()
+        {
+            Configuration.ConfigureGrabOnEnter();
+        }
+
+        /// <summary>
+        /// Called after <see cref="TargetValidity"/> has been changed.
+        /// </summary>
+        protected virtual void OnAfterTargetValidityChange()
+        {
+            Configuration.ConfigurePointerRules();
+        }
+
+        /// <summary>
+        /// Called after <see cref="RaycastRules"/> has been changed.
+        /// </summary>
+        protected virtual void OnAfterRaycastRulesChange()
+        {
+            Configuration.ConfigurePointerRules();
+        }
+    }
+}
